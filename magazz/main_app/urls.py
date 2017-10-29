8c4e36de0e5fdad1936a7fa7acd84753dd7eb2e7
tonixhaker <tonixhaker@gmail.com>
2017-10-29 19:54:54 +0200

    Reviews

--- magazz/main_app/urls.py
+++ magazz/main_app/urls.py
@@ -7,8 +7,10 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view()),
     url(r'^products/$', views.ProductsView.as_view(), name='products'),
     url(r'^addprod/$', views.ProductsAdd.as_view(), name='addprod'),
+    url(r'^prodetail/(?P<pk>\d+)/$', views.ProductDetail.as_view(), name='prodetail'),
     url(r'^categories/$', views.CategoriesView.as_view(), name='categories'),
     url(r'^addcat/$', views.CategoriesAdd.as_view(), name='addcat'),
+    url(r'^review/$', views.add_review, name='review'),
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- magazz/main_app/views.py
+++ magazz/main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
+from django.views.generic.edit import FormMixin
 from .forms import *
 # Create your views here.
 
@@ -12,6 +13,31 @@ class ProductsView(ListView):
     template_name = "products.html"
     model = Product
     context_object_name = 'products'
+
+
+class ProductDetail(ListView):
+    template_name = "detailprod.html"
+    model = Product
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        return Product.objects.filter(id=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetail, self).get_context_data(**kwargs)
+        productobj = Product.objects.filter(id=self.kwargs['pk'])[0]
+        context['feedbacks'] = productobj.review_set.all()
+        return context
+
+
+def add_review(request):
+    prod = Product.objects.only('id').get(id=request.POST['prodid'])
+
+    Review.objects.create(username=request.POST['username'],
+                          rewiewtext=request.POST['rewiewtext'],
+                          rate=int(request.POST['rate']),
+                          product=prod)
+    return redirect('prodetail',int(request.POST['prodid']))
 
 
 class ProductsAdd(CreateView):
