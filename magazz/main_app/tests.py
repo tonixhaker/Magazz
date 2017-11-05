27e372c20d6ea8530395b7782f2dd77962d0d936
tonixhaker <tonixhaker@gmail.com>
2017-11-05 19:12:12 +0200

    Fixtures + some tests

--- magazz/main_app/tests.py
+++ magazz/main_app/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from .models import Product, Category
+from .models import Product, Category, Review
 import datetime
+
 
 class ProductTestCase(TestCase):
     def setUp(self):
@@ -20,3 +21,25 @@ class ProductTestCase(TestCase):
         self.assertEqual(testobj.price, 666)
         self.assertEqual(testobj.category, Category.objects.get(title="test_cat"))
         self.assertEqual(testobj.publishdate, datetime.date(2017, 11, 5))
+
+
+class ReviewTestCase(TestCase):
+    def setUp(self):
+        cat = Category.objects.create(title="test_cat")
+        prod = Product.objects.create(name="test_prod",
+                               photo="test.jpg",
+                               descript="testdescr",
+                               price=666,
+                               category=cat,
+                               publishdate="2017-11-05")
+        Review.objects.create(username="testuser",
+                              rewiewtext="some review text",
+                              rate=3,
+                              product=prod)
+
+    def test_rewiew_created(self):
+        """Animals that can speak are correctly identified"""
+        testobj = Review.objects.get(username="testuser")
+        self.assertEqual(testobj.rewiewtext, "some review text")
+        self.assertEqual(testobj.rate, 3)
+        self.assertEqual(testobj.product, Product.objects.get(name="test_prod"))
